acb373dc861f60b2b702c106f70418640b57c6f8
Sobi Mohammet <sobimohammet@gmail.com>
2023-02-26 02:02:49 +0100

    Complete working rock_paper_scissors game

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-/* creation of a function that randomly return either rock, paper or scissors */
+let a = 0;
+let b = 0;
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let options = ["rock", "paper", "scissors"];
 function getComputerChoice(options) {
     return options[Math.floor(Math.random()*options.length)];
 }
-let options = ["rock", "paper", "scissors"];
 let computerSelection = getComputerChoice(options);
-
-
-/* creation of a function that returns the player's choice(rock, paper or scissors) */
 function getPlayerChoice() {
+    let playerChoice = prompt("Enter your selection");
     return playerChoice;
 }
-let playerChoice = prompt("Enter your choice(rock, paper or scissors)").toLowerCase();
-let playerSelection = getPlayerChoice();
-
-
-/* creation of a function that plays a single round of rock paper scissors */
-function playRound(playerSelection, computerSelection) {
-    if ((playerSelection=="scissors" && computerSelection=="paper") |
-    (playerSelection=="rock" && computerSelection=="scissors") |
-    (playerSelection=="paper" && computerSelection=="rock")) {
-        return playerWinStatement;
+let selection = getPlayerChoice();
+let playerSelection = selection.toLocaleLowerCase();
+let draw = "It's a draw, try again!";
+let win = "You win!" + " " + playerSelection + " beats " + computerSelection;
+let lose = "You lose! " + computerSelection + " beats " + playerSelection;
+let wrongSelection = "Wrong input! please try again!";
+function singleRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return draw;
     }
-
-    else if (playerSelection==computerSelection) {
-        return drawStatement;
+    else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
+        return win;
     }
-
-    else if ((playerSelection=="paper" && computerSelection=="scissors") |
-    (playerSelection=="scissors" && computerSelection=="rock") |
-    (playerSelection=="rock" && computerSelection=="paper")) {
-        return computerWinStatement;
+    else if ((playerSelection === "scissors" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "rock" && computerSelection === "paper")) {
+        return lose;
     }
-
     else {
-        return "Please enter either rock, paper or scissors it does not matter whether or not it in lower case";
+        return wrongSelection;
     }
 }
-let playerWinStatement = "You win! " + playerSelection + " beats " + computerSelection;
-let computerWinStatement = "You lose! " + computerSelection + " beats " + playerSelection;
-let drawStatement = "It is a draw!";
-
-
-/* creation of a function that plays five times */
-function game() {
-    for (let i=0; i<5; i++) {
-        return playRound(playerSelection, computerSelection);
+console.log(singleRound(playerSelection, computerSelection));
+if (singleRound(playerSelection, computerSelection) === win) {
+    a = a + 1;
+}
+else if (singleRound(playerSelection, computerSelection) === lose) {
+    b = b + 1;
+}
     }
 }
-console.log(game());
+game();
+if (a > b) {
+    console.log("You win! score is You: " + a + " Computer: " + b);
+}
+else if (a < b) {
+    console.log("You lose! score is You: " + a + " Computer: " + b);
+}
+else if (a === b) {
+    console.log("It's a draw! score is  You: " + a + " Computer: " + b);
+}
